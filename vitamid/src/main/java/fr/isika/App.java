42ba12cb4aa92c2ff3c4a14441fe8dd2b8b24dbd
vitamidev <vitamidev@yahoo.com>
2022-03-28 16:52:54 +0200

    commit1 App.java

---
+++ vitamid/src/main/java/fr/isika/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.isika;
+
+/**
+ * @author admin
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
